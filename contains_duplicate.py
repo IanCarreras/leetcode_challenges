1cfd863c557d1058cac9abdfea21ce0ca786bb74
Ian <mus5002@aol.com>
2020-09-16 19:29:35 -0400

    added comments to contains_duplicate.py

--- contains_duplicate.py
+++ contains_duplicate.py
@@ -1,3 +1,11 @@
+# a hashtable will run in O(n) here for time and space
+# more efficient if size is unknown
+
+# if the given arrays will never be very large 
+# it could be more efficient to sort the array 
+# then iterate through the array comparing two adjacent
+# values if they are the same return true
+
 class Solution:  
     def containsDuplicate(self, nums: List[int]) -> bool:
         hashtable = {}
